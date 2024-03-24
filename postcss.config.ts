453410929aa9fbbbb412b39c28b6dbfdc1eb848a
Takeru KIDO <keidy2463@gmail.com>
2024-03-25 00:51:41 +0900

    Fix applying tailwindcss

--- postcss.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{css,js,ts,jsx,tsx}"],
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
-
-module.exports = config;
