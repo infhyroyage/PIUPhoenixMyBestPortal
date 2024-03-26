8a50d7f32432e34789b0f3807652404804291709
Takeru KIDO <keidy2463@gmail.com>
2024-03-26 22:08:51 +0900

    Add github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: process.env.GITHUB_ACTIONS
+    ? "/PIUPhoenixMyBestPortal/"
+    : undefined,
+  basePath: process.env.GITHUB_ACTIONS ? "/PIUPhoenixMyBestPortal" : undefined,
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
