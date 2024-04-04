b0fba313e08d0dacbf4cc181db7491ccbb874da6
Takeru KIDO <keidy2463@gmail.com>
2024-04-04 21:46:28 +0900

    Add NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import NavBar from "./NavBar";
 
 export const metadata: Metadata = {
-  title: "Pump It Up Phoenix My Best List",
+  title: "Phoenix My Best List",
   description: "Pump It Up Phoenix My Best List (Over Lv.20)",
 };
 
@@ -16,9 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex min-h-screen flex-col items-center bg-white dark:bg-gray-800">
-          <h1 className="mt-4 text-3xl font-bold text-gray-900 dark:text-white">
-            Pump It Up Phoenix My Best List
-          </h1>
+          <NavBar />
           <div className="my-8">{children}</div>
         </div>
       </body>
