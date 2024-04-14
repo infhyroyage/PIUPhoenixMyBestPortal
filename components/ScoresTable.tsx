d010b2dc3b2b5519bb801027f240fa2067cdae7f
Takeru KIDO <keidy2463@gmail.com>
2024-04-14 18:58:49 +0900

    Update responsible UI in ScoresTable

--- components/ScoresTable.tsx
+++ components/ScoresTable.tsx
@@ -3,18 +3,21 @@ import { Score } from "@/services/types";
 
 export default function ScoresTable({ scores }: { scores: Score[] }) {
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div className="relative overflow-x-auto rounded-lg shadow-md">
       <table className="text-left text-xs text-gray-500 rtl:text-right dark:text-gray-400">
         <thead className="bg-gray-50 uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="max-w-40 py-2 pl-4 pr-2">
+            <th
+              scope="col"
+              className="max-w-32 py-2 pl-1 pr-1 md:max-w-96 md:pl-4 md:pr-2"
+            >
               Song
             </th>
-            <th scope="col" className="py-2 pr-4" />
-            <th scope="col" className="py-2 pr-4">
+            <th scope="col" className="py-2 pr-1 md:pr-4" />
+            <th scope="col" className="py-2 pr-1 md:pr-4">
               Lv.
             </th>
-            <th scope="col" className="py-2 pr-4">
+            <th scope="col" className="py-2 pr-1 md:pr-4">
               Score
             </th>
           </tr>
@@ -27,23 +30,23 @@ export default function ScoresTable({ scores }: { scores: Score[] }) {
             >
               <th
                 scope="row"
-                className="max-w-40 overflow-hidden text-ellipsis py-2 pl-4 pr-2 font-medium text-gray-900 dark:text-white"
+                className="max-w-32 overflow-hidden text-ellipsis py-2 pl-1 pr-1 font-medium text-gray-900 md:max-w-96 md:pl-4 md:pr-2 dark:text-white"
               >
                 {score.songName}
               </th>
-              <td className="py-2 pr-4">
+              <td className="py-2 pr-1 md:pr-4">
                 <Image
                   src={score.thumbnailImgSrc}
                   alt={score.songName}
-                  width={(32 * 700) / 393}
-                  height={32}
+                  width={(48 * 700) / 393}
+                  height={48}
                 />
               </td>
-              <td className="py-2 pr-4">{score.stepType}</td>
-              <td className="py-2 pr-4">
+              <td className="py-2 pr-1 md:pr-4">{score.stepType}</td>
+              <td className="py-2 pr-1 md:pr-4">
                 <div>
                   <div className="h-4 text-center">{score.score || ""}</div>
-                  <div className="mt-2 flex space-x-1">
+                  <div className="mt-2 flex">
                     <div className="relative h-6 w-auto min-w-[calc(24px*289/130)]">
                       {score.gradeImgSrc && (
                         <Image
