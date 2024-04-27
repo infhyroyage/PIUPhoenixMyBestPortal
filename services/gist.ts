dfc087b30446954c4654e2da6205259abb04281b
Takeru KIDO <keidy2463@gmail.com>
2024-04-28 01:45:34 +0900

    Support pagination at listGistInfo

--- services/gist.ts
+++ services/gist.ts
@@ -12,21 +12,39 @@ export async function listGistInfo(
   lv?: Lv,
 ): Promise<GistInfo[]> {
   // List All Gists
-  // TODO: Pagination
-  // https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28
-  const res: Response = await fetch(
-    "https://api.github.com/gists?per_page=100",
-    {
+  const gistInfoList: GistInfo[] = [];
+  let url: string = "https://api.github.com/gists";
+  let isRemained: boolean = true;
+  while (isRemained) {
+    // Get Partial Gists
+    const res: Response = await fetch(url, {
       headers: {
         Accept: "application/vnd.github+json",
         Authorization: `Bearer ${gistPat}`,
         "X-GitHub-Api-Version": "2022-11-28",
       },
-    },
-  );
+    });
+    const data: GistInfo[] = await res.json();
+    gistInfoList.push(...data);
+
+    // Extract Next Gists Getting URL for Pagination
+    const linkHeader: string | null = res.headers.get("link");
+    if (!linkHeader) {
+      isRemained = false;
+      break;
+    }
+    const matchedResult: RegExpMatchArray | null = linkHeader.match(
+      /(?<=<)([\S]*)(?=>; rel="next")/i,
+    );
+    if (!matchedResult || matchedResult.length === 0) {
+      isRemained = false;
+      break;
+    }
+    url = matchedResult[0];
+  }
 
   // Filter PIUPhoenixMyBestPortal Gists
-  return (await res.json()).filter((gist: GistInfo) => {
+  return gistInfoList.filter((gist: GistInfo) => {
     if (gist.description !== "PIUPhoenixMyBestPortal") return false;
     if (lv) {
       const filename: string = `${lv}.json`;
